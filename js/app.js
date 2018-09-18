38878bd3c419ef63ad4a564f4bb55b4ce154c17e
Philip Hoover <philhoov90@gmail.com>
2018-09-18 05:40:03 -0600

    Data Structures

---
+++ js/app.js
@@ -0,0 +1,26 @@
+console.log('its too early for this shit')
+
+// * A light switch that can be either on or off -  Boolean variable, true is on, false is off
+const lightSwitchOn = true;
+// * A user's email address - string
+const email = "usersemail@GA.com";
+// * A spaceship with a hull, laser blasters, tractor beam, and warp drive - array
+const spaceship = ["hull", "laser blasters", "tractor beam", "warp drive"];
+// * A list of student names from our class - array
+const classRoll = ["Philip", "Kelly", "James", "Max", "Jack", "Deirdre", "Liz"];
+// * A list of student names from our class, each with a location - array of objects
+const studentLocation = [
+    {name: "Philip", location: "his bed"},
+    {name: "Jack", location: "not his bed"},
+    {name: "Ryan", location: "in the kitchen making another giant cookie"}
+]
+// * A list of student names from our class, each with a location and each with a list of favorite tv shows - array of objects, 
+// eacn with an array for the list of favorite shows
+const studentLocationAndFavShows = [
+    {name: "Phil", location: "Cheesman park", 
+    favoriteShows: ["Star Trek", "The Office", "Rick & Morty"]},
+    {name: "Liz", location: "somewhere",
+    favoriteShows: ["The New Girl", "Friends", "Naked and Afraid"]},
+    {name: "Donny", location: "somewhere else",
+    favoriteShows: ["The Wire", "The Daily Show", "American Horror Story"]}
+]
